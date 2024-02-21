b08d4edb7ff96446f6b65c1833a4ca78fe0701d3
karacsm <karacsony.marton99@gmail.com>
2024-02-21 12:00:24 +0100

    small fixes

--- scripts/modules/msgutil.py
+++ scripts/modules/msgutil.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import json
+import uuid
 import pooltool as pt
 import threading
 from pooltool import System, Cue
@@ -24,7 +25,6 @@ class InvalidMessageError(Exception):
         self.msg = msg
         super().__init__()
 
-
 class MessageCode(IntEnum):
     ConnectionClosed = 0
     Login = 1
@@ -43,64 +43,74 @@ class Message:
         encoded_msg_code = int.to_bytes(int(self.code), length=_MSG_CODE_SIZE, byteorder=_MSG_CODE_BYTEORDER)
         return encoded_msg_code + json.dumps(self.data).encode('utf-8')
 
+    def _decode_data(self):
+        raise NotImplementedError(f'{self} instance must implement _decode_data!')
+
 class ConnectionClosedMessage(Message):
     def __init__(self):
-        super().__init__(MessageCode.ConnectionClosed, {})
+        super().__init__(MessageCode.ConnectionClosed, None)
+
+    def _decode_data(self):
+        pass
 
 class LoginMessage(Message):
-    def __init__(self, secret: str = ''):
-        data = {'secret' : secret}
+    def __init__(self, name: str, secret: Optional[uuid.UUID] = None):
+        self.name = name
+        self.secret = secret
+        data = {'name' : name}
+        if secret is not None:
+            data['secret'] = secret.hex
         super().__init__(MessageCode.Login, data)
 
-    @property
-    def secret(self) -> str:
-        return self.data['secret']
+    def _decode_data(self):
+        self.name = self.data['name']
+        if 'secret' in self.data.keys():
+            self.secret = uuid.UUID(hex = self.data['secret'])
+        else:
+            self.secret = None
 
 class LoginSuccessMessage(Message):
-    def __init__(self, player_id: str, secret: str):
-        data = {'player_id' : player_id, 'secret' : secret}
+    def __init__(self, player_id: str, secret: uuid.UUID):
+        self.player_id = player_id
+        self.secret = secret
+        data = {'player_id' : player_id, 'secret' : secret.hex}
         super().__init__(MessageCode.LoginSuccess, data)
 
-    @property
-    def player_id(self) -> str:
-        return self.data['player_id']
-
-    @property
-    def secret(self) -> str:
-        return self.data['secret']
+    def _decode_data(self):
+        self.player_id = self.data['player_id']
+        self.secret = uuid.UUID(hex = self.data['secret'])
 
 class LoginFailedMessage(Message):
     def __init__(self, reason: str):
+        self.reason = reason
         data = {'reason' : reason}
         super().__init__(MessageCode.LoginFailed, data)
 
-    @property
-    def reason(self):
-        return self.data['reason']
+    def _decode_data(self):
+        self.reason = self.data['reason']
 
 class YourTurnMessage(Message):
     def __init__(self, system: System, shot_constraints: ShotConstraints, break_shot: bool):
+        self.system = system
+        self.shot_constraints = shot_constraints
+        self.break_shot = break_shot
         data = {'system' : pt.serialize.conversion.converters['json'].unstructure(system),
                 'shot_constraints' : pt.serialize.conversion.converters['json'].unstructure(shot_constraints),
                 'break_shot' : break_shot}
         super().__init__(MessageCode.YourTurn, data)
 
-    @property
-    def system(self):
-        raw = self.data['system']
-        return pt.serialize.conversion.converters['json'].structure(raw, System)
-    
-    @property
-    def shot_constraints(self):
-        raw = self.data['shot_constraints']
-        return pt.serialize.conversion.converters['json'].structure(raw, ShotConstraints)
-
-    @property
-    def break_shot(self):
-        return self.data['break_shot']
+    def _decode_data(self):
+        raw_system = self.data['system']
+        raw_shot_constraints = self.data['shot_constraints']
+        self.system = pt.serialize.conversion.converters['json'].structure(raw_system, System)
+        self.shot_constraints = pt.serialize.conversion.converters['json'].structure(raw_shot_constraints, ShotConstraints)
+        self.break_shot = self.data['break_shot']
 
 class MakeShotMessage(Message):
     def __init__(self, cue: Cue, cue_ball_pos: BallPosition = None, shot_call: ShotCall = None):
+        self.cue = cue
+        self.cue_ball_pos = cue_ball_pos
+        self.shot_call = shot_call
         data = {'cue' : pt.serialize.conversion.converters['json'].unstructure(cue)}
         if cue_ball_pos is not None:
             data['cue_ball_pos'] = cue_ball_pos.__dict__
@@ -108,36 +118,27 @@ class MakeShotMessage(Message):
             data['shot_call'] = shot_call.__dict__
         super().__init__(MessageCode.MakeShot, data)
 
-    @property
-    def cue(self):
-        raw = self.data['cue']
-        return pt.serialize.conversion.converters['json'].structure(raw, Cue)
-
-    @property
-    def cue_ball_pos(self):
+    def _decode_data(self):
+        raw_cue = self.data['cue']
+        self.cue = pt.serialize.conversion.converters['json'].structure(raw_cue, Cue)
         if 'cue_ball_pos' in self.data.keys():
-            return BallPosition(**self.data['cue_ball_pos'])
+            self.cue_ball_pos = BallPosition(**self.data['cue_ball_pos'])
         else:
-            return None
-
-    @property
-    def shot_call(self):
+            self.cue_ball_pos = None
         if 'shot_call' in self.data.keys():
-            return ShotCall(**self.data['shot_call'])
+            self.shot_call = ShotCall(**self.data['shot_call'])
         else:
-            return None
+            self.shot_call = None
 
 class GameOverMessage(Message):
     def __init__(self, winner: str, scores: dict):
+        self.winner = winner
+        self.scores = scores
         super().__init__(MessageCode.GameOver, {'winner' : winner, 'scores' : scores})
-    
-    @property
-    def winner(self):
-        return self.data['winner']
 
-    @property
-    def scores(self):
-        return self.data['scores']
+    def _decode_data(self):
+        self.winner = self.data['winner']
+        self.scores = self.data['scores']
 
 _message_translation_dict = {
     MessageCode.ConnectionClosed : ConnectionClosedMessage,
@@ -155,44 +156,19 @@ def decode_msg(msg: bytes) -> Message:
         data = json.loads(msg[_MSG_CODE_SIZE:])
         dmsg = Message(code, data)
         dmsg.__class__ = _message_translation_dict[dmsg.code]
+        dmsg._decode_data()
         return dmsg
     except Exception: #catch almost everything to protect againts invalid messages
         raise InvalidMessageError(msg)
 
-def send_msg(conn: socket.socket, msg: Message, timeout: Optional[float] = None):
-    conn.settimeout(timeout)
-    data = msg.encode()
-    header = int.to_bytes(len(data), _MSG_HEADER_SIZE, byteorder=_MSG_HEADER_BYTEORDER)
-    msg = header + data
-    conn.sendall(msg)
-
-def _receive_nbytes(conn: socket.socket, bytecount: int, maxbufsize) -> bytes:
-    remaining_length = bytecount
-    data = b''
-    while remaining_length > 0:
-        if remaining_length <= maxbufsize:
-            bufsize = remaining_length
-        else:
-            bufsize = maxbufsize
-        data_chunk = conn.recv(bufsize)
-        if len(data_chunk) == 0:
-            raise ConnectionClosedError('Connection closed before all bytes received!')
-        remaining_length = remaining_length - len(data_chunk)
-        data = data + data_chunk
-    return data
-
-def receive_msg(conn: socket.socket, maxbufsize: int = 1024, timeout: Optional[float] = None) -> Message:
-    try:
-        conn.settimeout(timeout)
-        header = _receive_nbytes(conn, _MSG_HEADER_SIZE, maxbufsize)
-        msg_len = int.from_bytes(bytes = header, byteorder=_MSG_HEADER_BYTEORDER)
-        msg = _receive_nbytes(conn, msg_len, maxbufsize)
-        return decode_msg(msg)
-    except ConnectionClosedError:
-        return ConnectionClosedMessage()
-
-#performs async socket IO
 class MessageBuffer:
+    """
+    Performs async IO on non-blocking socket.
+
+    Retreive messages from buffer using peek_msg(), pop_msg() or await_msg().
+    Send message with push_msg()
+    Call stop() before closing the socket.
+    """
     def __init__(self, conn: socket.socket, update_freq: int = 200):
         assert(conn.getblocking() == False)
         self._conn = conn
